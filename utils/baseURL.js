299cb07e53972e3b9884b1f43834de141724b1e3
klaudiamilosia <klaudiamiloszewska100@gmail.com>
2024-02-27 17:57:53 +0100

    commit

--- utils/baseURL.js
+++ utils/baseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://192.168.1.14:7247';
-export const websiteURL = 'http://192.168.1.14:3000'
+export const baseURL = 'http://localhost:7247';
+export const websiteURL = 'http://localhost:3000'
